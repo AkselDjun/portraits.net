0c6428299e757289be7b9db2efb325df9844e68a
AkselDjun <akseldjun@gmail.com>
2020-05-31 16:44:59 +0300

    create accordion & burger-menu & grop

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,10 @@ import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 import calc from "./modules/calc";
 import pictureSize from "./modules/pictureSize";
+import accordion from "./modules/accordion";
+import burger from './modules/burger';
+import drop from './modules/drop';
+
 
 window.addEventListener("DOMContentLoaded", () => {
     "use strict";
@@ -10,4 +14,7 @@ window.addEventListener("DOMContentLoaded", () => {
     sliders(".feedback-slider-item", "horixontal", ".main-prev-btn", ".main-next-btn");
     calc("#size", "#material", "#options", ".calc-price");
     pictureSize(".sizes-block");
+    accordion(".accordion-heading", ".accordion-block");
+    burger(".burger-menu", ".burger");
+    drop();
 });
